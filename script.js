30c28019e4366637914b0808943df82c3149afe6
snowsanta <45097716+snowsanta@users.noreply.github.com>
2020-01-28 07:05:27 +0530

    dev_site

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var back_cols = ["rgb(150,206,180)","rgb(255,238,173)"]
+var height = window.innerHeight;
+
+
+
 function drawimg(image, context, x, y, w = 0, h = 0){
     if(!image.complete){
         setTimeout(function(){
@@ -12,6 +17,32 @@ function drawimg(image, context, x, y, w = 0, h = 0){
     context.drawImage(image, x, y, window.innerWidth, window.innerHeight);
 }
 
+CanvasRenderingContext2D.prototype.roundedRect = function (x, y, width, height, radius){
+    // Because the function is added to the context prototype
+    // the 'this' variable is actually the context
+    
+    // Save the existing state of the canvas so that it can be restored later
+    this.save();
+    
+        // Translate to the given X/Y coordinates
+        this.translate(x, y);
+
+        // Move to the center of the top horizontal line
+        this.moveTo(width / 2,0);
+        
+        // Draw the rounded corners. The connecting lines in between them are drawn automatically
+        this.arcTo(width,0,width,height, Math.min(height / 2, radius));
+        this.arcTo(width, height, 0, height, Math.min(width / 2, radius));
+        this.arcTo(0, height, 0, 0, Math.min(height / 2, radius));
+        this.arcTo(0, 0, radius, 0, Math.min(width / 2, radius));
+
+        // Draw a line back to the start coordinates
+        this.lineTo(width / 2,0);
+
+    // Restore the state of the canvas to as it was before the save
+    this.restore();
+}
+
 var beach_back = new Image();
 beach_back.src = './renders/beach_back.jpg';
 var sand = new Image();
@@ -20,6 +51,10 @@ var forage_li = new Image();
 forage_li.src = './renders/forage_light.png';
 var forage = new Image();
 forage.src = './renders/forage_dark.png';
+var lem = new Image();
+lem.src = "./renders/lemonade_sill.png";
+var crowd = new Image();
+crowd.src = "./renders/crowd.png";
 
 //layer1
 
@@ -48,13 +83,28 @@ canv_back.height = canv_back.scrollHeight;
 canv_back.width = canv_back.scrollWidth;
 var ctx_back = canv_back.getContext('2d');
 
+var canv_anim = document.getElementById("content_anim");
+canv_anim.height = canv_anim.scrollHeight;
+canv_anim.width = canv_anim.scrollWidth;
+var ctx_anim = canv_anim.getContext('2d');
+
+var canv_back_props = document.getElementById("content_back_prop");
+canv_back_props.height = canv_back_props.scrollHeight;
+canv_back_props.width = canv_back_props.scrollWidth;
+var ctx_back_props = canv_back_props.getContext('2d');
+
 
 drawimg(beach_back, ctx, 0, -150);
 drawimg(sand, ctx, 0, 90);
 
+ctx_back.fillStyle = back_cols[0];
+ctx_back.fillRect(0,0,window.innerWidth,window.innerHeight);
+ctx_back.fillRect(0,window.innerHeight * 2,window.innerWidth,window.innerHeight);
+ctx_back.fillRect(0,window.innerHeight * 4,window.innerWidth,window.innerHeight);
 
-ctx_back.fillRect(0,0,canv_back.scrollHeight,canv_back.scrollWidth);
-
+ctx_back.fillStyle = back_cols[1];
+ctx_back.fillRect(0,window.innerHeight * 1,window.innerWidth,window.innerHeight);
+ctx_back.fillRect(0,window.innerHeight * 3,window.innerWidth,window.innerHeight);
 
 // layer2
 
@@ -72,13 +122,13 @@ var Animation_forage = function(){
 }
 
 // Define update loop on the "Animation" prototype
+var run__anim = 1;
 var flg = 1;
-
-
+var flg3 = 1;
 Animation_forage.prototype.update = function() {
-    
-    
-    
+
+     
+
     ctx2.clearRect(0, 0, canv2.width, canv2.height);
     ctx3.clearRect(0, 0, canv3.width, canv3.height);
 
@@ -102,21 +152,27 @@ Animation_forage.prototype.update = function() {
     this.forage_l.y -= (this.forage_l.speed * animationDeltaTime) * window.innerHeight / 2;
     this.forage_l.fac += this.forage_l.speed * animationDeltaTime;
 
-
-    console.log(this.forage_d.fac);
     //schedule and make constraint
-    if(this.forage_d.fac >= 1.2 && flg){
+
+    //for first stop
+    if(this.forage_d.fac > 1.2 && flg){
         flg = 0;
+        console.log(flg);
         return;
-    };
-        
-    if((this.forage_d.fac <= 1.2 || this.forage_d.fac > 2) && !flg){
+    }
+    
+    //for stop when goes out
+    else if((this.forage_d.fac > 2 || this.forage_d.fac < 1.2) && (!flg)){
+        console.log(this.forage_d.fac);
+
         this.forage_d.speed = this.forage_d.speed * -1;
         this.forage_l.speed = this.forage_l.speed * -1;
         return;
-    };
+    }
 
-    window.requestAnimationFrame(this.boundUpdate);
+    else{
+        window.requestAnimationFrame(this.boundUpdate);
+    };
 }
 
 
@@ -210,8 +266,123 @@ animation_sun.update();
 // drawimg(forage, ctx2, -100, -100, 1.2, 1.2);
 
 
-// layer3
+// content layer animations
 
+
+//woofer
+ctx_back_props.beginPath();
+ctx_back_props.fillStyle = 'rgba(255,255,255,.5)';
+ctx_back_props.roundedRect(-300, height / 2 - 300 - 15, 700 + 15, 200, 15);
+ctx_back_props.fill();
+ctx_back_props.closePath();
+
+
+ctx_back_props.beginPath();
+ctx_back_props.fillStyle = back_cols[0] ;
+ctx_back_props.roundedRect(-300, height / 2 - 300 + 15, 700 - 15, 200, 15);
+ctx_back_props.fill();
+ctx_back_props.closePath();
+
+
+ctx_back_props.beginPath();
+ctx_back_props.clearRect(-300, height / 2 - 200, 800, 400, 50);
+ctx_back_props.fillStyle = 'rgba(255,255,255,.5)';
+ctx_back_props.roundedRect(-300, height / 2 - 200, 800, 400, 50);
+ctx_back_props.fill();
+ctx_back_props.closePath();
+
+
+ctx_back_props.clearRect(-50,height / 2 - 130,700,50);
+ctx_back_props.fillStyle = back_cols[0];
+ctx_back_props.beginPath();
+ctx_back_props.arc(330, height / 2 + 60, 75, 0, 2 * Math.PI);
+ctx_back_props.closePath();
+ctx_back_props.fill();
+ctx_back_props.beginPath();
+ctx_back_props.arc(430, height / 2 - 160, 25, 0, 2 * Math.PI);
+ctx_back_props.closePath();
+ctx_back_props.fill();
+ctx_back_props.clearRect(0,height / 2 + 15,150,100)
+// ctx_back_props.beginPath();
+// ctx_back_props = "red";
+// ctx_back_props.arc(500,height / 2 - 130 , 100, 0, 2 * Math.PI);
+// ctx_back_props.fill();
+// ctx_back_props.closePath();
+
+
+
+
+//lemonade
+drawimg(lem, ctx_back_props, 0, height);
+
+//stage
+drawimg(crowd, ctx_back_props, 0, height*2);
+
+
+// var Animation_cont_layer = function(){
+//     // Explicitly bind update()'s 'this' context and cache in instance property
+//     this.boundUpdate = this.update.bind(this);
+
+//     this.forage_d = {img : forage, x: 0, y: 0, fac: 1, speed : .7};
+//     this.forage_l = {img : forage_li, x: 0, y: 50, fac: 1, speed : 1.5};
+
+//     // Track frame time
+//     this.lastAnimationTime = 0;
+// }
+
+// // Define update loop on the "Animation" prototype
+// var run__anim = 1;
+// var flg = 1;
+// var flg3 = 1;
+// Animation_cont_layer.prototype.update = function() {
+
+     
+
+//     ctx2.clearRect(0, 0, canv2.width, canv2.height);
+//     ctx3.clearRect(0, 0, canv3.width, canv3.height);
+
+//     // Calculate time since last frame
+//     var currentAnimationTime = Date.now();
+//     var animationDeltaTime   = (currentAnimationTime - (this.lastAnimationTime || Date.now())) / 3000;
+
+//     // Reset time for next frame
+//     this.lastAnimationTime = currentAnimationTime;
+
+//     // Draw
+//     drawimg(this.forage_d.img , ctx2, this.forage_d.x, this.forage_d.y, this.forage_d.fac, this.forage_d.fac);
+//     drawimg(this.forage_l.img , ctx3, this.forage_l.x, this.forage_l.y, this.forage_l.fac, this.forage_l.fac);
+
+//     // Update props
+//     this.forage_d.x -= (this.forage_d.speed * animationDeltaTime) * window.innerWidth / 2;
+//     this.forage_d.y -= (this.forage_d.speed * animationDeltaTime) * window.innerHeight / 2;
+//     this.forage_d.fac += this.forage_d.speed * animationDeltaTime;
+
+//     this.forage_l.x -= (this.forage_l.speed * animationDeltaTime) * window.innerWidth / 2;
+//     this.forage_l.y -= (this.forage_l.speed * animationDeltaTime) * window.innerHeight / 2;
+//     this.forage_l.fac += this.forage_l.speed * animationDeltaTime;
+
+//     //schedule and make constraint
+
+//     //for first stop
+//     if(this.forage_d.fac > 1.2 && flg){
+//         flg = 0;
+//         console.log(flg);
+//         return;
+//     }
+    
+//     //for stop when goes out
+//     else if((this.forage_d.fac > 2 || this.forage_d.fac < 1.2) && (!flg)){
+//         console.log(this.forage_d.fac);
+
+//         this.forage_d.speed = this.forage_d.speed * -1;
+//         this.forage_l.speed = this.forage_l.speed * -1;
+//         return;
+//     }
+
+//     else{
+//         window.requestAnimationFrame(this.boundUpdate);
+//     };
+// }
 
 
 
