1fb119aa780074f4521ad89025627604e0652b40
dipanshu231099 <dipanshu231099@gmail.com>
2020-03-11 09:35:21 +0530

    created js to tell total amount

--- total_find.js
+++ total_find.js
@@ -2,23 +2,14 @@
 var y = [2000,200,500,600,300,300,300,1500,200,1500,200,100,200,0,0,0,400,400,0,2000,150,150,0,100,500,500,500,500,500,500,500,300,0,0,200,0,1200,1200,1200];
 
 var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  
-  var daycontainer=document.getElementById("days")
-  var hourcontainer=document.getElementById("hours")
-  var mincontainer=document.getElementById("mins")
-  
-  daycontainer.innerHTML=days + "<br>Days"
-  hourcontainer.innerHTML=hours + "<br>Hours"
-  mincontainer.innerHTML=minutes + "<br>Mins"
+    var events = document.getElementsByClassName('event_checkbox');
+    var price = document.getElementsByClassName('payment_button')[0];
+    var team_sizes = document.getElementsByClassName('tentacles');
+    var to_pay =0;
+    for(var i=0;i<events.length;i++){
+        if(events[i].checked){
+            to_pay+=y[i]*team_sizes[i].value;
+        }
+    }
+    price.innerHTML = "Pay: ₹ "+to_pay +"/-";
 },100)
